add7c0d92f128df6c3bc5b476d3a43a36ea58b17
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2022-10-27 21:30:08 -0400

    fix: add labels to spin dials

--- components/SpinDial.tsx
+++ components/SpinDial.tsx
@@ -3,32 +3,49 @@ import useInterval from "../util/chooks.js";
 // import Logo from "../public/images/logo.png";
 
 const digits: Array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-function strmap(str, cb) { return Array.prototype.map.call(str, cb); }
-function RollingNumber({num}: {num: int}) {
-    return (<>
-        <div className="relative left-0 transition ease-in-out duration-500" style={{transform: `translateY(${-5 * num}em)`}}>
-            {
-                digits.map(num => (<>
-                    <div className="h-20 w-16 flex justify-center items-center font-bold text-5xl">
-                        {num}
-                    </div>
-                </>))
-            }
-        </div>
-    </>)
+function strmap(str, cb) {
+  return Array.prototype.map.call(str, cb);
 }
-export default function SpinDial({num, digits, descriptor}: {num: int, digits: int, descriptor: string}) {
-    let strnum = String(num);
-    const display: string = strnum.padStart(digits, "0");
+function RollingNumber({ num }: { num: int }) {
+  return (
+    <>
+      <div
+        className="relative left-0 transition ease-in-out duration-500"
+        style={{ transform: `translateY(${-5 * num}em)` }}
+      >
+        {digits.map((num) => (
+          <>
+            <div className="h-20 w-16 flex justify-center items-center font-bold text-5xl">
+              {num}
+            </div>
+          </>
+        ))}
+      </div>
+    </>
+  );
+}
+export default function SpinDial({
+  num,
+  digits,
+  descriptor,
+}: {
+  num: int;
+  digits: int;
+  descriptor: string;
+}) {
+  let strnum = String(num);
+  const display: string = strnum.padStart(digits, "0");
 
-    return (<div className="mx-2 flex flex-row">
-        {
-            strmap(display, digit => (
-                <div className="float-left h-20 w-16 bg-gradient-to-b from-slate-200 via-transparent to-slate-200 place-content-center text-center overflow-hidden">
-                    <RollingNumber num={Number(digit)} />
-                </div>
-            ))
-        }
-        </div>
-    )
+  return (
+    <div className="mx-2 flex flex-col">
+      <div className="flex">
+        {strmap(display, (digit) => (
+          <div className="float-left h-20 w-16 bg-gradient-to-b from-slate-200 via-transparent to-slate-200 place-content-center text-center overflow-hidden">
+            <RollingNumber num={Number(digit)} />
+          </div>
+        ))}
+      </div>
+      <p className="text-xl text-center">{descriptor}</p>
+    </div>
+  );
 }

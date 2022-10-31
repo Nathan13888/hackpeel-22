b79fcb2452d7bf5dfd1d2462c678cd3a161ce908
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2022-10-31 11:24:59 -0400

    fix: change socials

--- util/config.js
+++ util/config.js
@@ -1,5 +1,5 @@
-const instagramLink = 'https://www.instagram.com/yourusername'
-const linkedinLink = 'https://www.linkedin.com/in/yourusername'
+const instagramLink = 'https://www.instagram.com/hackpeel'
+const linkedinLink = 'https://www.linkedin.com/company/hack-peel/'
 const discordLink = 'https://discord.gg/z75rxjTTDC'
 
 export { instagramLink, linkedinLink, discordLink }
